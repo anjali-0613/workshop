a2af7d6dc5d311b21e3d36a451d8bd92d09f0469
anjali-0613 <ammuluanjali2@gmail.com>
2025-03-21 11:41:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Hello World')
+st.write('This is a simple Streamlit app.')
+	st.button('hello Streamlit app.')
+		st.text('hello Streamlit app.')
+st.text_input('please enter your name:')
