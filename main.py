f3243e1b81a94d462aeed454dbe20e4c72758326
anjali-0613 <ammuluanjali2@gmail.com>
2025-03-21 12:45:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import streamlit as st
 
-st.title('Hello World')
+
+
+st.title('Hello anjali')
 st.write('This is a simple Streamlit app.')
-	st.button('hello Streamlit app.')
-		st.text('hello Streamlit app.')
-st.text_input('please enter your name:')
+
+if st.button('hello Streamlit app.'):
+    st.text('Hello, Streamlit!')
+
+name = st.text_input('Please enter your name:')
+if name:
+    st.write(f'Hello, {name}!')
+
+file = st.file_uploader('Please upload a file:', type=['txt', 'csv'])
+if file:
+    st.write('Thanks for uploading a file!')
